5457d36f9c6972372a1a35e5e8fceb3fc4e0a8ae
Martin Törnqvist <m.tornq@gmail.com>
2016-08-15 22:50:07 +0200

    Added example config for an Emacs "project" (.dir_locals.el and .clang_complete)

---
+++ emacs-project-examples/.dir_locals.el
@@ -0,0 +1,23 @@
+((nil
+  . ((eval . (progn
+               (require 'projectile)
+               (setq company-clang-arguments (delete-dups (append
+                                                           company-clang-arguments
+                                                           (list
+                                                            "-std=c++14"
+                                                            (concat "-I" (projectile-project-root) "include")
+                                                            (concat "-I" (projectile-project-root) "rl_utils/include")
+                                                            (concat "-I" (projectile-project-root) "json/2.0.2/include")
+                                                            "-I/usr/include/SDL2"
+                                                            ))))))))
+ (c++-mode
+  (whitespace-line-column . 80)
+  (eval ignore-errors
+        (add-hook 'write-contents-functions
+                  (lambda ()
+                    (delete-trailing-whitespace)
+                    nil))
+        (require 'whitespace)
+        (whitespace-mode 0)
+        (whitespace-mode 1)))
+ )
